eaea98ba11476413cc3e2189ee5f730e8f6df312
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-05-03 13:22:02 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCIhuqR7JK8Duhzdrc4x4dlcpleXAn9o-4",
+    authDomain: "ahmad-1971e.firebaseapp.com",
+    databaseURL: "https://ahmad-1971e-default-rtdb.firebaseio.com",
+    projectId: "ahmad-1971e",
+    storageBucket: "ahmad-1971e.firebasestorage.app",
+    messagingSenderId: "274320575302",
+    appId: "1:274320575302:web:a877709f4bbd0292f87347",
+    measurementId: "G-80CWJW2SC9"
+  },
+
+  firebaseRoot: "cashier_units_pro_ahmad_1971e_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_ahmad_1971e_885766842"
+  ]
+};
